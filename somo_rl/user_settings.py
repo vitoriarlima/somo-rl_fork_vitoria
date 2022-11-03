6e8b6912e7033a5a87e11803fcf5be50c64c9651
vitoriarlima <vito.rodrigueslima@gmail.com>
2022-11-03 14:59:06 -0400

    adding new small changes

--- somo_rl/user_settings.py
+++ somo_rl/user_settings.py
@@ -1,1 +1,1 @@
-EXPERIMENT_ABS_PATH = '/home/vitoria/Desktop/thesis_project_new1/somo-rl_fork_vitoria/experiments'
+EXPERIMENT_ABS_PATH = "/home/vitoria/Desktop/thesis_project_new1/somo-rl_fork_vitoria/experiments"
